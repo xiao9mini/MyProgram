9ee66306b569ef9a00bb3c6ebe721559324e3ca2
xiao9mini <xiao9mini@gmail.com>
2024-06-11 15:58:09 +0000

    xx

--- monitor/dataMonitoring.sh
+++ monitor/dataMonitoring.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
 
 # 用于监控网卡流量，当流量达到设定值时，自动关机
 in=${1:-500}  # 进站 默认500
 out=${2:-150} # 出站 默认150
 all=${3:-500} # 进出 默认500
 
-ens=$(ip link | grep ens |   awk -F ':' '{ print $2}' | awk '{print $1}')
+ens=$(ip link | grep ens | awk -F ':' '{ print $2}' | awk '{print $1}')
 
 curPath=$(dirname $(readlink -f "$0")) # 文件所在目录
-cd $(dirname $(readlink -f "$0"))      # CD文件所在目录
+if [[ "$curPath" == /proc/* ]]; then
+    curPath=$(pwd)
+fi
+cd $curPath # CD文件所在目录
 
 sudo apt install vnstat bc -y 2>&1 >/dev/null
 sudo sed -i 's/;UnitMode 0/UnitMode 1/' /etc/vnstat.conf
@@ -47,18 +51,21 @@ if [ \$allr -ge \$all ]; then
     sudo /usr/sbin/shutdown -h now
 fi
 
-" > "$curPath/check.sh"
+" >"$curPath/check.sh"
 
 cmd="/bin/bash $curPath/check.sh"
-cron_job="*/5 * * * * $cmd > $curPath/check.log 2>&1"   # 每5分钟执行一次
+cron_job="*/5 * * * * $cmd > $curPath/check.log 2>&1" # 每5分钟执行一次
 # 如果存在则删除cmd
 # echo "$current_crontab" | grep -v "$cmd" | crontab -
 
 # 添加新的cmd
-(crontab -l | grep -v -F "$cmd" ; echo "$cron_job") | crontab -
+(
+    crontab -l | grep -v -F "$cmd"
+    echo "$cron_job"
+) | crontab -
 
 crontab -l
 echo "监控网卡流量，当流量达到设定值时，自动关机。（5分钟）"
-in=$in      # 进站 默认500
-out=$out    # 出站 默认150
-all=$all    # 进出 默认500"
+in=$in   # 进站 默认500
+out=$out # 出站 默认150
+all=$all # 进出 默认500"
